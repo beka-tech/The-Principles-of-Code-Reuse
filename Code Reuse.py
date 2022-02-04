b5ee5eee09127d447d6a26b22baeb09e73f3c59f
beka aman <63505760+beka833@users.noreply.github.com>
2022-02-04 18:45:39 +0300

    Create Code Reuse.py

---
+++ Code Reuse.py
@@ -0,0 +1,11 @@
+# REPLACE THIS STARTER CODE WITH YOUR FUNCTION
+def month_days(month,days):
+
+  print( month + " has " + days + " days.")
+
+month_days("june","30")
+month_days("july","31")
+
+
+
+
